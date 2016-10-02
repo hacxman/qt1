5fc2f6db75ad230e9a6c0c8e0d8b45c41a1a81d8
Maros Zatko <mzatko@redhat.com>
2016-10-02 17:33:33 +0200

    button click action

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source config.sh
-rcc -binary qt0.qrc -o qt0.rcc
-LD_LIBRARY_PATH=$HSQT_PATH/qws/bin/ ghc -package-db $HSQT_PATH/.cabal-sandbox/x86_64-linux-ghc-7.8.4-packages.conf.d/ -I$HSQT_PATH/qws/include -lqtc_core  -L$HSQT_PATH/qws/bin -lqtc_gui -lqtc_script -lqtc_network  -lqtc_opengl -lqtc_tools qt0.hs
+rm qt1.rcc
+rcc -binary qt1.qrc -o qt1.rcc
+LD_LIBRARY_PATH=$HSQT_PATH/qws/bin/ ghc -package-db $HSQT_PATH/.cabal-sandbox/x86_64-linux-ghc-7.8.4-packages.conf.d/ -I$HSQT_PATH/qws/include -lqtc_core  -L$HSQT_PATH/qws/bin -lqtc_gui -lqtc_script -lqtc_network  -lqtc_opengl -lqtc_tools qt1.hs
--- qt1.hs
+++ qt1.hs
@@ -10,8 +10,10 @@ import Qtc.Core.Base
 import Qtc.Core.QIODevice
 import Qtc.Enums.Core.QIODevice
 import Qtc.Core.QFile
+import Qtc.ClassTypes.Gui
 import Qtc.Gui.Base
 import Qtc.Gui.QApplication
+import Qtc.Gui.QLineEdit
 import Qtc.Gui.QWidget
 import Qtc.Core.QTextStream
 import Qtc.ClassTypes.Tools
@@ -19,13 +21,27 @@ import Qtc.Tools.QUiLoader
 import Qtc.Tools.QUiLoader_h
 import Qtc.Classes.Script
 
+connect_actions ui = do
+  ui <- qSubClass $ qCast_QWidget ui
+  btn <- findChild ui ("<QPushButton*>", "pushButton")
+  line <- findChild ui ("<QLineEdit*>", "lineEdit")
+  connectSlot btn "clicked()" ui "pushButtonClicked()" $ btnClicked line
+
+btnClicked :: QLineEdit String -> QWidget () -> IO ()
+btnClicked line w = do
+  setText line "lol"
+  return ()
+
 main = do
   app <- qApplication ()
-  rok <- registerResource "qt0.rcc"
+  rok <- registerResource "qt1.rcc"
   loader <- qUiLoader ()
   uifile <- qFile ":/main.ui"
   ui <- load loader uifile
   close uifile ()
+
+  connect_actions ui
+
   qshow ui ()
   ok <- qApplicationExec ()
   return ()
